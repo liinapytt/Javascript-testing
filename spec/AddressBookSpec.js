5b2562add08ab42078ee14c1db299a7d4847bee0
liinapytt <liina.rebassoo@khk.ee>
2019-02-28 19:14:28 +0200

    14 Removing redundant code

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,9 +1,15 @@
 //lisan uue kirje, milles defineerida projektis vajalikke objekte ning muutujaid
 describe("Address Book", function() {
+  //defineerin muutujad, mida läbivalt failis kasutan
+  var addressBook, thisContact;
+
+  //lisan funktsiooni, millega iga funktsiooni eel aadressraamatu ja kontakti objektid välja kutsutakse
+  beforeEach(function() {
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
   it("should be able to add a contact", function() {
-    //kutsun aadressiraamatu ja kontakti objektid välja
-    var addressBook = new AddressBook(),
-      thisContact = new Contact();
     //lisan meetodi, millega kontakte aadressraamatusse lisada
     addressBook.addContact(thisContact);
     //eeldan, et päringu peale tagastakse mulle lisatud kontakt
@@ -12,9 +18,6 @@ describe("Address Book", function() {
 
   //lisan uue kirjelduse kontakti kustutamiseks
   it("should be able to delete a contact", function() {
-    //kutsun aadressiraamatu ja kontakti objektid välja
-    var addressBook = new AddressBook(),
-      thisContact = new Contact();
     //lisan meetodi, millega kontakte aadressraamatusse lisada
     addressBook.addContact(thisContact);
     //lisan meetodi, millega lisatud kontakti kustutada saan
